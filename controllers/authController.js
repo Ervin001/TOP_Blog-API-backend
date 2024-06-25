9665c9a2b25d5759890185e26bf8d13fd6fad91d
Ervin Gonzalez <83973082+Ervin001@users.noreply.github.com>
2024-06-25 00:31:52 -0500

    Removed unnecessary routes in auth for creating user, since they should exists in users route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
 
-//  Get Sign up
-exports.getSignup = asyncHandler(async (req, res) => {
-  res.json({ message: 'Get Sign up' });
-});
-
-//  Post Sign up
-exports.postSignup = asyncHandler(async (req, res) => {
-  res.json({ message: 'Post Sign up' });
-});
-
 //  Get Sign in
 exports.getLogin = asyncHandler(async (req, res) => {
   res.json({ message: 'get Log in' });
--- routes/api.js
+++ routes/api.js
@@ -6,11 +6,6 @@ const users_controller = require('../controllers/usersController');
 const auth_controller = require('../controllers/authController');
 
 // -------------- Auth -------------
-// Get sign up form
-router.get('/auth/sign-up', auth_controller.getSignup);
-
-// Post sign up form
-router.post('/auth/sign-up', auth_controller.postSignup);
 
 // Get sign in form
 router.get('/auth/login', auth_controller.getLogin);
